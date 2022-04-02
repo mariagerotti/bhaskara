2617bbf6b0b2e6329c8a2018df64f7c4d9967786
mariagerotti <99097680+mariagerotti@users.noreply.github.com>
2022-04-02 18:16:53 -0300

    alterações código

--- bhaskara.js
+++ bhaskara.js
@@ -1,30 +1,30 @@
-function delta(a, b, c) {
+function calcularDelta(a, b, c){
     return b*b - 4*a*c
 }
-function calcX1(delta, a, b) {
+function calcularX1(delta, a, b){
     return (-b + Math.sqrt(delta)) / (2*a)
 }
-function calcX2(delta, a, b) {
-    return (-b + Math.sqrt(delta)) / (2*a)
+function calcularX2(delta, a, b){
+    return (-b - Math.sqrt(delta)) / (2*a)
 }
 function bhaskara(a, b, c){
-    const valorDelta = delta(a, b, c)
-    let x1 = 'inexistente', x2 = 'inexistente'
-    if (valorDelta === 0) {
-        x1 = calcX1(valorDelta, a, b)
-        x2 = 'inexistente'
+    const valorDelta = calcularDelta(a, b, c)
+    let x1 = 'Inexistente', x2 = 'Inexistente'
+    if (valorDelta === 0){
+        x1 = calcularX1(valorDelta, a, b)
+        x2 = 'Inexistente'
     }
-    else if (valorDelta > 0) {
-        x1 = calcX1(valorDelta, a, b)
-        x2 = calcX2(valorDelta, a, b)
+    else if (valorDelta > 0){
+        x1 = calcularX1(valorDelta, a, b)
+        x2 = calcularX2(valorDelta, a, b)
     }
-     return {x1: x1, x2: x2}
+    return {x1: x1, x2: x2}
 }
 
 function handlebtnCalcular() {
-    const a = parseInt (document.getElementById('a').value)
-    const b = parseInt (document.getElementById('b').value)
-    const c = parseInt (document.getElementById('c').value)
+    const a = parseInt (document.getElementById('valorA').value)
+    const b = parseInt (document.getElementById('valorB').value)
+    const c = parseInt (document.getElementById('valorC').value)
     const resultado = bhaskara(a, b, c)
     document.getElementById('resultadoX1').innerHTML = 'x1: ' + resultado.x1
     document.getElementById('resultadoX2').innerHTML = 'x2: ' + resultado.x2
