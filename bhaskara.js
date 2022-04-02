05cff22f38232f188fa16b56741837f63a8e83e9
mariagerotti <99097680+mariagerotti@users.noreply.github.com>
2022-04-02 18:04:32 -0300

    alterações codigo

--- bhaskara.js
+++ bhaskara.js
@@ -8,7 +8,7 @@ function calcX2(delta, a, b) {
     return (-b + Math.sqrt(delta)) / (2*a)
 }
 function bhaskara(a, b, c){
-    const valorDelta = calcularDelta(a, b, c)
+    const valorDelta = delta(a, b, c)
     let x1 = 'inexistente', x2 = 'inexistente'
     if (valorDelta === 0) {
         x1 = calcX1(valorDelta, a, b)
@@ -18,9 +18,17 @@ function bhaskara(a, b, c){
         x1 = calcX1(valorDelta, a, b)
         x2 = calcX2(valorDelta, a, b)
     }
-     return { x1: x1, x2: x2}
+     return {x1: x1, x2: x2}
 }
 
-console.log(bhaskara(1, -5, 4))
-console.log(bhaskara(2, 5, 4))
-console.log(bhaskara(1, 4, 4))
+function handlebtnCalcular() {
+    const a = parseInt (document.getElementById('a').value)
+    const b = parseInt (document.getElementById('b').value)
+    const c = parseInt (document.getElementById('c').value)
+    const resultado = bhaskara(a, b, c)
+    document.getElementById('resultadoX1').innerHTML = 'x1: ' + resultado.x1
+    document.getElementById('resultadoX2').innerHTML = 'x2: ' + resultado.x2
+}
+
+
+document.getElementById('btnCalcular').addEventListener('click', handlebtnCalcular)
